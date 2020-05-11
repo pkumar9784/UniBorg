3527ff53424df5ec50c0243cbc784e77cfee1cb7
23rdmaxim <64205435+23rdmaxim@users.noreply.github.com>
2020-05-11 15:06:22 +0530

    Uploaded New Plugin

---
+++ stdplugins/hearts.py
@@ -0,0 +1,19 @@
+"""Original written by @UniBorg
+\nFurther edited by @maxim_233
+\n\nCommand .hrts"""
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.hrts", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("💙💛💓💔💘💕💜💚💝💞💟"))
+	for _ in range(100):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
